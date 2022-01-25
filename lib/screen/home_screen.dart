0a34683288860ba45581a9510be60c23b303fe09
sk25469 <sk25469@gmail.com>
2022-01-25 21:55:55 +0530

    ui changes

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  static final routeName = '/home';
+  static const routeName = '/home';
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Text('HomeScreen');
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Feeds'),
+        elevation: 0,
+        backgroundColor: theme.backgroundColor,
+      ),
+      body: Center(
+        child: Text('Home'),
+      ),
+    );
   }
 }
