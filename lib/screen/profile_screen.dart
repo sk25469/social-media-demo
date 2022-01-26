a946b033043331faf07793a81375391f6726f72d
sk25469 <sk25469@gmail.com>
2022-01-26 14:57:51 +0530

    minor changes

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,36 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:social_media/provider/auth_provider.dart';
-import 'package:social_media/screen/login_page.dart';
 
-class ProfileScreen extends ConsumerWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final _auth = ref.watch(authenticationProvider);
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  bool _isLoading = false;
+
+  void loading() {
+    setState(() {
+      _isLoading = !_isLoading;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
+      body: Consumer(builder: (context, ref, _) {
+        final _auth = ref.read(authenticationProvider);
+        Future<void> _onPressedFunction() async {
+          loading();
+          await _auth.signOut().whenComplete(
+                () => _auth.authStateChange.listen(
+                  (event) async {
+                    if (event == null) {
+                      loading();
+                      return;
+                    }
+                  },
+                ),
+              );
+        }
+
+        return Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('Profile Screen'),
-            ElevatedButton(
-              onPressed: () async {
-                await _auth.signOut().whenComplete(
-                      () => Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(
-                          builder: (context) => const LoginPage(),
-                        ),
-                      ),
-                    );
-              },
-              child: const Text("Sign Out"),
-            ),
+            _isLoading
+                ? const CircularProgressIndicator()
+                : ElevatedButton(
+                    onPressed: _onPressedFunction,
+                    child: const Text("Sign Out"),
+                  ),
           ],
-        ),
-      ),
+        );
+      }),
     );
   }
 }
